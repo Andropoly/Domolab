c874bf5ef2f7f08397033242712569ae42b827b6
Munier Louis <30860289+lmunier@users.noreply.github.com>
2018-12-18 16:54:30 +0100

    Fixe look of the layout with image and text below.

--- mobile/src/main/java/ch/epfl/andropoly/domolab/ItemAdapter.java
+++ mobile/src/main/java/ch/epfl/andropoly/domolab/ItemAdapter.java
@@ -1,5 +1,6 @@
 package ch.epfl.andropoly.domolab;
 
+import android.media.Image;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
@@ -8,6 +9,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Arrays;
 import java.util.List;
@@ -41,14 +44,14 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder
     }
 
     public class ItemViewHolder extends RecyclerView.ViewHolder {
-        private final Button imgButton;
+        private final Button roomButton;
         private Pair<String, String> currentPair;
 
         public ItemViewHolder(final View itemView) {
             super(itemView);
-            imgButton = ((Button) itemView.findViewById(R.id.imageButton));
+            roomButton = (Button) itemView.findViewById(R.id.btn_room);
 
-            imgButton.setOnClickListener(new View.OnClickListener() {
+            roomButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     // TODO
@@ -58,25 +61,27 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder
 
         public void display(Pair<Object, String> pair) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                ImageView imgButton = (ImageView) itemView.findViewById(R.id.img_room);
+                TextView txt_room = (TextView) itemView.findViewById(R.id.txt_room);
+                txt_room.setText(pair.second);
+
                 switch (pair.second) {
                     case ("Kitchen"):
-                        imgButton.setBackgroundResource(R.drawable.table);
+                        imgButton.setImageResource(R.drawable.table);
                         break;
                     case ("Room"):
-                        imgButton.setBackgroundResource(R.drawable.bed);
+                        imgButton.setImageResource(R.drawable.bed);
                         break;
                     case ("Restroom"):
-                        imgButton.setBackgroundResource(R.drawable.restroom);
+                        imgButton.setImageResource(R.drawable.restroom);
                         break;
                     case ("Living room"):
-                        imgButton.setBackgroundResource(R.drawable.sofa);
+                        imgButton.setImageResource(R.drawable.sofa);
                         break;
                     default:
                         break;
                 }
             }
-
-            imgButton.setText(pair.second);
         }
     }
 }
