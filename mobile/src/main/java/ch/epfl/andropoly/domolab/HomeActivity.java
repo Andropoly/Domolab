5fe994df4389414cc9999bb0f6b7c34be94d60d3
BM <martial.bernard-michel@epfl.ch>
2019-01-13 20:27:05 +0100

    added wear service but useless and started database

--- mobile/src/main/java/ch/epfl/andropoly/domolab/HomeActivity.java
+++ mobile/src/main/java/ch/epfl/andropoly/domolab/HomeActivity.java
@@ -1,12 +1,21 @@
 package ch.epfl.andropoly.domolab;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Arrays;
 import java.util.List;
@@ -21,6 +30,10 @@ public class HomeActivity extends AppCompatActivity {
     private List<String> list_fav = Arrays.asList(
             "Kitchen","Restroom","Restroom","Restroom"
     );
+    private String userID;
+    private String profileKey;
+    private DatabaseReference profileRef;
+    private FirebaseDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +42,25 @@ public class HomeActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         if (intent.getExtras() != null) {
-            //TODO: Save user id
+            userID = intent.getExtras().getString("USERID");
+            profileKey = intent.getExtras().getString("PROFILEKEY");
         }
+
+        database = FirebaseDatabase.getInstance();
+        profileRef = database.getReference("profiles");
+        profileRef.child(profileKey).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String homename_db = dataSnapshot.child("HomeName").getValue(String.class);
+                String userID_db = dataSnapshot.child("userID").getValue(String.class);
+                setLayoutWithDatabase(homename_db, userID_db);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Empty
+            }
+        });
 
         //private RecordingAdapter adapter;
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.list_rooms);
@@ -52,5 +82,14 @@ public class HomeActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(grid_layout);
 
         recyclerView.setAdapter(new ItemAdapter(list_rooms, false));
+
+
+
+    }
+
+    private void setLayoutWithDatabase(String homename, String userid) {
+        Toast.makeText(HomeActivity.this, "Welcome to your home: " + homename + " !",
+                Toast.LENGTH_LONG).show();
+        Log.d(TAG, "Welcome to your home: " + homename + " !");
     }
 }
