963bf78733db4eb825e74a4442b14946b858c98d
victor <victor.faraut@gmail.com>
2019-01-14 19:35:32 +0100

    Json reader

--- mobile/src/main/java/ch/epfl/andropoly/domolab/Tes2Activity.java
+++ mobile/src/main/java/ch/epfl/andropoly/domolab/Tes2Activity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.JsonReader;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -14,7 +15,9 @@ import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.Serializable;
+import java.io.IOException;
+
+import JsonUtilisties.myJsonReader;
 
 import MQTTsender.MqttDomolab;
 import MQTTsender.NotConnectedException;
@@ -39,14 +42,17 @@ public class Tes2Activity extends AppCompatActivity {
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                JSONObject obj = new JSONObject();
-
+                JSONObject obj = null;
                 try {
-                    obj.put("1",3.2);
-                    obj.put("2",3.2);
-                    mqttDomolab.sendJSONToTopic(obj,"test");
+                    obj = myJsonReader.jsonFromFile( Tes2Activity.this,"test.json");
+                } catch (IOException e) {
+                    e.printStackTrace();
                 } catch (JSONException e) {
                     e.printStackTrace();
+                }
+
+                try {
+                    mqttDomolab.sendJSONToTopic(obj,"test");
                 } catch (NotConnectedException e) {
                     e.printStackTrace();
                 }
