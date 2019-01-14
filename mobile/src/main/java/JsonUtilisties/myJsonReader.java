535a706cbdea52766c019b69e10fc28606431904
victor <victor.faraut@gmail.com>
2019-01-14 23:39:07 +0100

    Reading and writing in a internal file

--- mobile/src/main/java/JsonUtilisties/myJsonReader.java
+++ mobile/src/main/java/JsonUtilisties/myJsonReader.java
@@ -6,6 +6,8 @@ import android.view.View;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -20,8 +22,32 @@ public class myJsonReader {
         return json;
 
     }
+    private static String stringFromFileInternal(Context activityContext, String filename) throws IOException {
 
-    public static JSONObject jsonFromFile(Context activityContext, String filename) throws IOException, JSONException {
+        FileInputStream is = activityContext.openFileInput(filename);
+        int size = is.available();
+        byte[] buffer = new byte[size];
+        is.read(buffer);
+        is.close();
+        String json = new String(buffer, "UTF-8");
+        return json;
+    }
+
+    public static JSONObject jsonFromFileAsset(Context activityContext, String filename) throws IOException, JSONException {
         return new JSONObject(stringFromFile(activityContext, filename));
+    }
+    public static JSONObject jsonFromFileInternal(Context activityContext, String filename) throws IOException, JSONException {
+        return new JSONObject(stringFromFileInternal(activityContext, filename));
+    }
+
+    public static void jsonWriteFileInternal(Context activityContext, String filename, JSONObject obj){
+        FileOutputStream outputStream;
+        try {
+            outputStream = activityContext.openFileOutput(filename, Context.MODE_PRIVATE);
+            outputStream.write(obj.toString().getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
